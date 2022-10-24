da37f89d7adc5cda039594306879aa332da48635
rizwanar6296 <rizwanar6296@gmail.com>
2022-10-24 17:38:46 +0530

    mongodb atlas url updated

--- config/connections.js
+++ config/connections.js
@@ -3,7 +3,7 @@ const state={
    db:null
 }
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+    const url='mongodb+srv://rizwan:<password>@cluster0.u2hllmq.mongodb.net/test'
     const dbname='ekart'
           
     mongoClient.connect(url,(err,data)=>{
